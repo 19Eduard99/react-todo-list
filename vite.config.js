c8dd786eb293649334beba228eca178915ada885
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2025-03-24 18:04:54 +0200

    fix2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/react-todo-list/' : '/',
+    base:
+      process.env.NODE_ENV === 'production'
+        ? '/react-todo-list/'
+        : '/',
   };
 });
